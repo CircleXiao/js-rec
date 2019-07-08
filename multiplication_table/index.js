b131bd210fbdaa54420063c87247d30f14b3e08a
mmy <mmy_xize@163.com>
2019-07-08 20:04:44 +0800

    add 2 folders, 1 file

---
+++ multiplication_table/index.js
@@ -0,0 +1,8 @@
+for (var i=1; i<=9; i++) {
+    for (var j=1; j<=i; j++) {
+        for (var k=1; k<=j; k++) {
+            console.log(i + " * " + j + " = " + i * j + "\t");
+        }
+        console.log("\n");
+    }
+}
